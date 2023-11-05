8d11b297ec0cd760d634aaad2d4d8f5f0825fb78
Tarik Hastor <thastor1@etf.unsa.ba>
2023-11-05 19:16:15 +0100

    internationale

---
+++ src/MedunarodniBroj.java
@@ -0,0 +1,12 @@
+public class MedunarodniBroj extends TelefonskiBroj{
+    private String drzava, broj;
+    public MedunarodniBroj(String drzava, String broj) {
+        this.drzava = drzava; this.broj = broj;
+    }
+    @Override
+    public String ispisi() {
+        StringBuilder s = new StringBuilder(drzava);
+        s.append("/" + broj);
+        return s.toString();
+    }
+}
