7c27682c0857e5bbf2ff01b391ae98d92f5da7f5
Tarik Hastor <thastor1@etf.unsa.ba>
2023-11-05 19:12:44 +0100

    gradovi

---
+++ src/Gradovi.java
@@ -0,0 +1,10 @@
+public enum Grad {
+    SARAJEVO("033"), TUZLA("035"), ZENICA("032"),
+    MOSTAR("036"), BIHAC("037"), ORASJE("031"),
+    GORAZDE("038"), TRAVNIK("030"), LIVNO("034"),
+    SIROKI_BRIJEG("039"), BRCKO("049");
+    public String name;
+    private Grad(String name) { this.name = name; }
+    @Override public String toString() { return this.name; }
+}
+
