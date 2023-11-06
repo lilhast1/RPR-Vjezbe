e75c43e699c0c80fd2fe09b48fd76c8dda6042a7
Tarik Hastor <thastor1@etf.unsa.ba>
2023-11-06 09:21:29 +0100

    testovi

--- test/ImenikTest.java
+++ test/ImenikTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ImenikTest {
@@ -14,16 +16,44 @@ class ImenikTest {
         MobilniBroj m = new MobilniBroj(62, "556-443");
         imenik.dodaj("Fata", m);
         assertEquals("""
-1. Faruk - 033/221-158
-2. Fata - 061/556-443
+1.Faruk - 033/221-158
+2.Fata - 062/556-443
 """, imenik.naSlovo('F'));
     }
 
     @Test
     void izGrada() {
+        FiksniBroj f = new FiksniBroj(Grad.SARAJEVO, "710-000");
+        Imenik imenik = new Imenik();
+        imenik.dodaj("Azra", f);
+        f = new FiksniBroj(Grad.BIHAC, "111-111");
+        imenik.dodaj("Mirza", f);
+        f = new FiksniBroj(Grad.MOSTAR, "222-222");
+        imenik.dodaj("Maja", f);
+        f = new FiksniBroj(Grad.MOSTAR, "333-333");
+        imenik.dodaj("Zdravko Maminjo Mamic", f);
+        var g = imenik.izGrada(Grad.MOSTAR);
+        Iterator it = g.iterator();
+        assertEquals("Maja", it.next());
+        assertEquals("Zdravko Maminjo Mamic", it.next());
+        assert(!it.hasNext());
     }
 
     @Test
     void izGradaBrojevi() {
+        FiksniBroj f = new FiksniBroj(Grad.SARAJEVO, "710-000");
+        Imenik imenik = new Imenik();
+        imenik.dodaj("Azra", f);
+        f = new FiksniBroj(Grad.BIHAC, "111-111");
+        imenik.dodaj("Mirza", f);
+        f = new FiksniBroj(Grad.MOSTAR, "222-222");
+        imenik.dodaj("Maja", f);
+        f = new FiksniBroj(Grad.MOSTAR, "333-333");
+        imenik.dodaj("Zdravko Maminjo Mamic", f);
+        var g = imenik.izGradaBrojevi(Grad.MOSTAR);
+        Iterator<TelefonskiBroj> it = g.iterator();
+        assertEquals("036/222-222", it.next().ispisi());
+        assertEquals("036/333-333", it.next().ispisi());
+        assert(!it.hasNext());
     }
 }
