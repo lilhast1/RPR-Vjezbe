c41e834c691b548c6c21f2fc200631c1d762fe01
Tarik Hastor <thastor1@etf.unsa.ba>
2023-11-07 16:30:17 +0100

    package program

---
+++ src/progam/Main.java
@@ -0,0 +1,22 @@
+package progam;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //petlja
+        System.out.println("Dobro dosli u ETF imenik!");
+        var scn = new Scanner(System.in);
+        ArrayList<Imenik> imenici = new ArrayList<>();
+        while (1) {
+            System.out.println("Izaberite opciju");
+            System.out.println("[1] [unesi broj]");
+            System.out.println("[2] [kreiraj imenik]");
+            System.out.println("[3] [dodaj u imenik]");
+            System.out.println("[4] [opciono broj imenika][udji u imenik]");
+
+        }
+
+    }
+}
