20078409d12818cd3abddbff97dae457b1e4fd99
Tarik Hastor <thastor1@etf.unsa.ba>
2023-11-13 19:52:46 +0100

    xml init

---
+++ src/main/java/ba/unsa/etf/rpr/LaptopDaoXMLFile.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.rpr;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class LaptopDaoXMLFile implements LaptopDao {
+    @Override
+    public LaptopDao dodajLaptopUListu(Laptop laptop) {
+        return null;
+    }
+
+    @Override
+    public LaptopDao dodajLaptopUFile(Laptop laptop) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Laptop getLaptop(String cpu) {
+        return null;
+    }
+
+    @Override
+    public void napuniListu(ArrayList<Laptop> laptops) {
+
+    }
+
+    @Override
+    public void vratiPodatkeIzDatoteke() throws IOException {
+
+    }
+}
