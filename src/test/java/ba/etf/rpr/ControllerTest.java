ed68a79fff9a72f201ded8f9a7006ee72e9cd089
Tarik Hastor <thastor1@etf.unsa.ba>
2023-12-19 18:28:11 +0100

    dodat testfx

---
+++ src/test/java/ba/etf/rpr/ControllerTest.java
@@ -0,0 +1,33 @@
+package ba.etf.rpr;
+
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(ApplicationExtension.class)
+class ControllerTest {
+    @Start
+    public void start(Stage stage) {
+
+
+    }
+    @Test
+    void initialize() {
+
+    }
+
+    @Test
+    void end() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void select() {
+    }
+}
